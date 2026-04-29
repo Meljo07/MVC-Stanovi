50eb9352f8365729c1cfe6c35341fed05071bd98
Meljo07 <ivanmelih08@gmail.com>
2026-04-29 15:53:34 +0200

    miav mivav

--- MVC-Stanovi/wwwroot/js/site.js
+++ MVC-Stanovi/wwwroot/js/site.js
@@ -1,14 +1,28 @@
 ﻿function dodajStan() {
-    let url = document.getElementById("slikaInput").value;
+    alert("radi");
+
+
+    let slika = document.getElementById("slikaInput").value;
+    let lokacija = document.getElementById("lokacijaInput").value;
+    let povrsina = document.getElementById("povrsinaInput").value;
+    let cena = document.getElementById("cenaInput").value;
+    let adresa = document.getElementById("adresaInput").value;
 
     let maliDiv = document.createElement("div");
     maliDiv.className = "stan";
 
     let img = document.createElement("img");
-    img.src = url;
-    img.style.width = "100px";
+    img.src = slika;
+    img.style.width = "250px";
+
+    let tekst = document.createElement("p");
+    tekst.innerText =
+        "Lokacija: " + lokacija +
+        "\nPovršina: " + povrsina +
+        "\nCena: " + cena +
+        "\nAdresa: " + adresa;
 
     maliDiv.appendChild(img);
+    maliDiv.appendChild(tekst);
 
     document.getElementById("velikiDiv").appendChild(maliDiv);
-}
